6c165ae8f8445dbd2c6385c2d4fb895a367c08e0
AlanGustav0 <alangustavo946@gmail.com>
2022-03-16 10:38:14 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+/*
+    Criamos uma "fábrica de Daos", dessa forma essa classe vai expor um método que retorna o tipo da interface, mas internamente
+    ela irá instanciar uma implementação. Com isso não precisamos expor a implmentação, ou seja, como é feita a instancia dessa classe.
+    É também uma maneira de fazer uma injeção de dependência.
+
+ */
+public class DaoFactory {
+
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+    }
+}
